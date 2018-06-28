baa27865bce3f6ecce073cbb505040e91f83ab60
Yauheni Yarotski <yauheni_yarotski@me.com>
2018-06-28 14:50:09 +0300

    test

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
       .package(url: "https://github.com/vapor/websocket.git", from: "1.0.0"),
       .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
       .package(url: "https://github.com/Hearst-DD/ObjectMapper.git", from: "3.2.0"),
-      .package(url: "https://github.com/BrettRToomey/Jobs.git", from: "1.1.2"),
+//      .package(url: "https://github.com/BrettRToomey/Jobs.git", from: "1.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Book2",
-  dependencies:  ["PerfectHTTPServer", "WebSocket", "ObjectMapper", "Jobs"]),
+  dependencies:  ["PerfectHTTPServer", "WebSocket", "ObjectMapper"]),
     ]
 )
